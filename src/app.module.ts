cb72adcfd1ed52ad27ca8f47ae89469525efe695
Сон Александр Владиславович <aleksandr.son@ent-en.com>
2023-05-31 14:17:25 +0500

    feat:start task, create file-structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './resourses/album/album.module';
+import { ArtistModule } from './resourses/artist/artist.module';
+import { FavouritesModule } from './resourses/favourites/favourites.module';
+import { TrackModule } from './resourses/track/track.module';
+import { UserModule } from './resourses/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, ArtistModule, FavouritesModule, TrackModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
+import * as process from 'process';
 
+const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const doc = await readFile(join(__dirname, '..', 'doc/api.yaml'), 'utf-8');
+  SwaggerModule.setup('doc', app, parse(doc));
+  await app.listen(PORT);
 }
 bootstrap();
